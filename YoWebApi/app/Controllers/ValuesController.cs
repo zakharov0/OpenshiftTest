e97e4ac0c69ebc42bf3baec4e395ef6f1cd9ded3
zakharov0 <zakharov@scanex.ru>
2017-03-09 18:21:55 +0300

    value plus id

--- YoWebApi/app/Controllers/ValuesController.cs
+++ YoWebApi/app/Controllers/ValuesController.cs
@@ -20,7 +20,7 @@ namespace YoWebApi.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value"+id;
         }
 
         // POST api/values
