816142d9d9a93f0cb634d5275aa7877c7e6e5538
Zakharov Mikhail <zakharov@scanex.ru>
2017-03-10 12:49:32 +0300

    data base refs

--- YoWebApi/app/Controllers/ValuesController.cs
+++ YoWebApi/app/Controllers/ValuesController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace VesselService
 {
